cab66cf53023ab1eb90c247214d2aeb5fcf74144
AndrewD14 <Leingod2005@gmail.com>
2017-11-15 12:48:13 -0600

    Added in the try catch block code.

--- js/liri-brains.js
+++ js/liri-brains.js
@@ -9,30 +9,36 @@ exports.processCommand = function(command, ranBefore){
 	if(command[0] == null)
 		console.log("Nothing was entered");
 	else{
-		switch(command[0].toLowerCase()){
-			case "my-tweets":
-				console.log("Calling twitter function");
-				twitter.getTweets();
-				break;
-			case "spotify-this-song":
-				console.log("Calling spotify function");
-				spotify.searchSongTitle(command[1]);
-				break;
-			case "movie-this":
-				console.log("Calling omdb api function");
-				omdb.getMovieInfo(command[1]);
-				break;
-			case "do-what-it-says":
-				if(ranBefore)
-					console.log("Already read in a file. Stopping to prevent a loop.");
-				else{
-					console.log("do what it says");
-					streamFile.readFile(command[1]);
-				}
-				break;
-			default:
-				console.log(command+" is not a valid command.");
-				break;
+		try{
+			switch(command[0].toLowerCase()){
+				case "my-tweets":
+					console.log("Calling twitter function");
+					twitter.getTweets();
+					break;
+				case "spotify-this-song":
+					console.log("Calling spotify function");
+					spotify.searchSongTitle(command[1]);
+					break;
+				case "movie-this":
+					console.log("Calling omdb api function");
+					omdb.getMovieInfo(command[1]);
+					break;
+				case "do-what-it-says":
+					if(ranBefore)
+						console.log("Already read in a file. Stopping to prevent a loop.");
+					else{
+						console.log("do what it says");
+						streamFile.readFile(command[1]);
+					}
+					break;
+				default:
+					console.log(command+" is not a valid command.");
+					break;
+			}
+		}
+		catch (err){
+			console.log("There was an error executing command: "+command[0]);
+			console.log(err);
 		}
 	}
 }
