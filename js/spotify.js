48e84a71a1e4c75ef468cc828860ca84fd3d47d6
AndrewD14 <Leingod2005@gmail.com>
2017-11-14 23:13:02 -0600

    Formatted the output of the spotify api. Added in the logic for the default spotify song.

--- js/spotify.js
+++ js/spotify.js
@@ -9,13 +9,50 @@ var spotify = new Spotify(keys.spotifyKeys);
 
 //searchs for a track using the provide song title
 exports.searchSongTitle = function(title){
-	//cehcks if no song title was inputted, if nothing, defaults to "The Sign" by Ace of Base
-	if(title == null)
-		title = "The Sign";
+	var parameters = {};
+	parameters.type = 'track';
 
-	spotify.search({ type: 'track', query: title }, function(err, data) {
+	//checks if no song title was inputted, if nothing, defaults to "The Sign" by Ace of Base
+	if(!title)
+		parameters.query = 'The Sign';
+	else
+		parameters.query = title;
+
+	spotify.search(parameters, function(err, data) {
 		if (err) throw err;
-	 
-		console.log(JSON.stringify(data)); 
+
+		var index = 0; //defaults to the first song pulled from spotify
+
+		//loops through to find the artist for the default test case
+		if(!title){
+			var found = false;
+			do{
+				for(i in data.tracks.items[index].artists){
+					if(data.tracks.items[index].artists[i].name.toLowerCase() == "ace of base")
+						found = true;
+				}
+				if(!found)
+					index++;
+			}while(!found && index < data.tracks.items.length);
+		}
+
+		console.log("Song: "+data.tracks.items[index].name)
+
+		console.log("Album: "+data.tracks.items[index].album.name);
+		
+		//grabs all artists and builds a string of their names together
+		var artists = "";
+		for(i in data.tracks.items[index].artists){
+			artists += data.tracks.items[index].artists[i].name;
+
+			if(i < data.tracks.items[index].artists.length-1)
+				artists += ", ";
+		}
+		console.log("Artists: "+artists);
+
+		if(data.tracks.items[index].preview_url)
+			console.log("Preview link: "+data.tracks.items[index].preview_url);
+		else
+			console.log("Preview link: Not available");
 	})
 }
